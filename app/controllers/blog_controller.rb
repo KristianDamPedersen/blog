bbc59c7e0db877ab0a0491051b1e4037731badab
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2025-09-10 20:48:17 +0200

    feat(ui): Adds blog posts to the nav bar

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,17 +1,23 @@
 class BlogController < ApplicationController
   def index
-    blog_folder = Rails.root.join("app/views/blog")
-
-    @articles = Dir.children(blog_folder)
-      .select { |f| f.ends_with?(".md") }
-      .map do |filename|
-        fm = FrontMatterParser::Parser.parse_file("#{blog_folder}/#{filename}").front_matter
-        fm.merge!(url: "blog/#{filename.delete_suffix(".md")}")
-        fm.with_indifferent_access
-      end
+    @articles = BlogController.get_meta
   end
 
   def article
     render params[:any].to_sym
+  end
+
+  ##
+  # Gets all the front matter meta data from the blog posts
+  def self.get_meta
+    blog_folder = Rails.root.join("app/views/blog")
+
+    Dir.children(blog_folder)
+      .select { |f| f.ends_with?(".md") }
+      .map do |filename|
+        fm = FrontMatterParser::Parser.parse_file("#{blog_folder}/#{filename}").front_matter
+        fm.merge!(url: "/blog/#{filename.delete_suffix(".md")}")
+        fm.with_indifferent_access
+      end
   end
 end
