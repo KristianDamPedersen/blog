71305dcdc2ad8ae041147ccd41f9883d271b68ed
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2025-09-10 19:53:09 +0200

    feat(ui): Adds initial container for md styling

--- lib/converters/markdown_to_html.rb
+++ lib/converters/markdown_to_html.rb
@@ -2,11 +2,24 @@ module Converters
   ##
   # This class handles converting Markdown to HTML.
   class MarkdownToHtml
+    TEMPLATE_CONTENT= <<-HTML
+    <div>
+      ${content}
+      <h1>Test</h1>
+    </div>
+    HTML
+
     ##
     # Converts markdown to html using the provided template?
     def call(template, source)
       markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-      "#{markdown.render(source).inspect}.html_safe"
+
+      # Remove front matter from markdown
+      sanitized_source = source.gsub(/---([\S\s]*)---/, "").strip
+
+      html = TEMPLATE_CONTENT.gsub("${content}", markdown.render(sanitized_source))
+
+      "#{html.inspect}.html_safe"
     end
   end
 end
