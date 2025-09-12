32f3f2ee9ca581e6f2028523d960cb61cb2ca824
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2025-09-12 09:24:00 +0200

    feat(ui): Adds the ability to style blog posts.
    This is just the skeleton, we still need to do the actual styling.

--- app/views/blog/my_first_article.md
+++ app/views/blog/my_first_article.md
@@ -13,3 +13,25 @@ icon-color: "text-red"
 ---
 # This is my first post, written purely in Markdown!
 Something something *italic* **bold**
+
+## Heading 2
+
+### Heading 3
+
+### Heading 4
+
+> Quote
+
+
+`def inline; end`
+
+```ruby
+def code_block
+  logger.info "this is from markdown"
+end
+
+```
+
+[This is a link](https://www.google.com) 
+
+![My image](https://f4.bcbits.com/img/a3329505383_16.jpg) 
--- lib/converters/markdown_to_html.rb
+++ lib/converters/markdown_to_html.rb
@@ -3,23 +3,30 @@ module Converters
   # This class handles converting Markdown to HTML.
   class MarkdownToHtml
     TEMPLATE_CONTENT= <<-HTML
-    <div>
-      ${content}
+      %%CONTENT%%
       <h1 class="text-fg">Test</h1>
-    </div>
     HTML
 
     ##
     # Converts markdown to html using the provided template?
     def call(template, source)
-      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+      markdown = Redcarpet::Markdown.new(
+        Redcarpet::Render::HTML,
+        fenced_code_blocks: true,
+        tables: true,
+        autolink: true,
+        highlight: true,
+        quote: true
+      )
 
       # Remove front matter from markdown
       sanitized_source = source.gsub(/---([\S\s]*)---/, "").strip
 
-      html = TEMPLATE_CONTENT.gsub("${content}", markdown.render(sanitized_source))
+      rendered_markdown = markdown.render(sanitized_source)
 
-      "#{html.inspect}.html_safe"
+      html = TEMPLATE_CONTENT.gsub("%%CONTENT%%", rendered_markdown)
+
+      html.html_safe
     end
   end
 end
